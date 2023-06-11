c53f855aaea272c9088ff14b8401ed2c88178cfd
Rob Hahn <robert@hahn.email>
2023-06-11 12:29:25 -0400

    .

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from "./$types";
-import { getFirelord } from "firelordjs";
-
-type OutputType = {};
-
-// @ts-ignore
-export const load: PageServerLoad<OutputType> = async (event) => {
-    return {};
-}
