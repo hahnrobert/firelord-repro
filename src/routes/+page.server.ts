8ab4b49274498da701ff081b36f2ff184a423445
Rob Hahn <robert@hahn.email>
2023-06-11 12:22:57 -0400

    .

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,22 +1,9 @@
 import type { PageServerLoad } from "./$types";
-import { getFirelord, getFirestore, type MetaTypeCreator } from "firelordjs";
-import * as firebase from "firebase/app";
+import { getFirelord } from "firelordjs";
 
 type OutputType = {};
 
-const app = firebase.getApp('app')
-const firestore = getFirestore(app)
-
-export type Users = MetaTypeCreator<{
-    name: string;
-},
-    'users'
->
-
-const users = getFirelord<Users>(firestore, 'users')
-
 // @ts-ignore
 export const load: PageServerLoad<OutputType> = async (event) => {
-    const user = users.doc('foobar');
     return {};
 }
